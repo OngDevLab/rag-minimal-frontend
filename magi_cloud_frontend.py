dacf0eb76dfc6e635925ed86895ef842be622b07
Ong <ongdevlab@gmail.com>
2025-01-05 17:10:59 -0500

    tested live db connection to table

--- magi_cloud_frontend.py
+++ magi_cloud_frontend.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("hello world")
 
 conn = st.connection("postgresql", type="sql")
-df = conn.query('SELECT 1;', ttl="10m")
+df = conn.query('SELECT error_message, response, prompt, feedback FROM public.magi_kb;', ttl="10m")
 st.dataframe(df)
