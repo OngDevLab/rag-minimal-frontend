e380af705a01f4840c7369f2f6424faef4af8300
Ong <ongdevlab@gmail.com>
2025-01-05 17:15:17 -0500

    made header look better

--- magi_cloud_frontend.py
+++ magi_cloud_frontend.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("Magi Error Management System Minimal Demo")
+st.title("Magi-Cloud")
+st.write("Error Management System Minimal Demo")
 
 conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT error_message, response, prompt, feedback FROM public.magi_kb;', ttl="10m")
