d814a7ad91434117f0e4cacfee34f3e14d473f1e
Ong <ongdevlab@gmail.com>
2025-01-05 17:43:29 -0500

    added autosizer

--- magi_cloud_frontend.py
+++ magi_cloud_frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid, GridOptionsBuilder, ColumnsAutoSizeMode
 import pandas as pd
 st.set_page_config(layout="wide")
 st.title("Magi-Cloud")
@@ -13,4 +13,4 @@ grid_builder.configure_selection(selection_mode="multiple", use_checkbox=True)
 grid_builder.configure_side_bar(filters_panel=True, columns_panel=False)
 grid_options = grid_builder.build()
 
-grid_return = AgGrid(data=df, gridOptions=grid_options, key='grid1')
+grid_return = AgGrid(data=df, gridOptions=grid_options, key='grid1', columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW)
