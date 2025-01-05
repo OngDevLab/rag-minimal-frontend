e2ee1c55ffa42ea40e694da86a77330c8a440e88
Ong <ongdevlab@gmail.com>
2025-01-05 17:18:35 -0500

    implementation test of aggrid

--- magi_cloud_frontend.py
+++ magi_cloud_frontend.py
@@ -1,8 +1,9 @@
 import streamlit as st
-
+from st_aggrid import AgGrid
+import pandas as pd
 st.title("Magi-Cloud")
 st.write("Error Management System Minimal Demo")
 
 conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT error_message, response, prompt, feedback FROM public.magi_kb;', ttl="10m")
-st.dataframe(df)
+grid_return = AgGrid(df)
