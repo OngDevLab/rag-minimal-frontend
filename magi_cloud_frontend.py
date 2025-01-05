422f7d8c888893fdd3777869a7e3f3b26d226a24
Ong <ongdevlab@gmail.com>
2025-01-05 18:34:14 -0500

    reversed id changes

--- magi_cloud_frontend.py
+++ magi_cloud_frontend.py
@@ -7,13 +7,13 @@ st.title("Magi-Cloud")
 st.write("Error Management System Minimal Demo")
 
 conn = st.connection("postgresql", type="sql")
-df = conn.query('SELECT error_message, response, prompt, feedback, id FROM public.magi_kb;', ttl="10m")
+df = conn.query('SELECT error_message, response, prompt, feedback FROM public.magi_kb;', ttl="10m")
 
 grid_builder = GridOptionsBuilder.from_dataframe(df)
 grid_builder.configure_selection(selection_mode="multiple", use_checkbox=True)
 grid_builder.configure_side_bar(filters_panel=True, columns_panel=False)
 grid_builder.configure_grid_options(autoHeight=True)
-grid_builder.configure_columns(['error_message', 'response','prompt','feedback','id'], wrapText=True, autoHeight=True, width=420)
+grid_builder.configure_columns(['error_message', 'response','prompt','feedback'], wrapText=True, autoHeight=True, width=420)
 grid_options = grid_builder.build()
 grid_return = AgGrid(data=df, gridOptions=grid_options, key='grid1', columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW)
 
