01cd8d820195933442b1aa2d7941b67f3eed5c20
Ong <ongdevlab@gmail.com>
2025-01-05 18:27:06 -0500

    added error message printout

--- magi_cloud_frontend.py
+++ magi_cloud_frontend.py
@@ -10,14 +10,16 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT error_message, response, prompt, feedback FROM public.magi_kb;', ttl="10m")
 
 grid_builder = GridOptionsBuilder.from_dataframe(df)
-grid_builder.configure_selection(selection_mode="single", use_checkbox=True)
+grid_builder.configure_selection(selection_mode="multiple", use_checkbox=True)
 grid_builder.configure_side_bar(filters_panel=True, columns_panel=False)
 grid_builder.configure_grid_options(autoHeight=True)
 grid_builder.configure_columns(['error_message', 'response','prompt','feedback'], wrapText=True, autoHeight=True, width=420)
 grid_options = grid_builder.build()
 grid_return = AgGrid(data=df, gridOptions=grid_options, key='grid1', columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW)
 
-selected_row = grid_return['selected_rows']
+selected_rows = grid_return['selected_rows']
 
-if selected_row is not None:
-    st.write("row selected")
+if selected_rows is not None:
+    for index, row in selected_rows.iterrows():
+        error_message = row['error_message']
+        st.write(error_message)
